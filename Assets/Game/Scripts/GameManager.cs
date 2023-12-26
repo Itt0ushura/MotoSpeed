42190dda71b95803e34a34998f41b21261062dd0
Itt0ushura <balashov580@gmail.com>
2023-12-26 17:55:32 +0200

    camera following motorbike

--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject motorBike; //gameobj of bike to manipulate with it
+    private Camera _camera;
+    public float CameraSpeed; // var for t component in Vector3.Lerp
     void Start()
     {
-        
+        _camera = Camera.main;
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //CameraFollowRaw();
+        //CameraFollowLerpBad();
+        CameraFollowLerbGood();
+    }
+    void CameraFollowRaw() //test feature
+    {
+        Vector3 cameraPos = new Vector3(motorBike.transform.position.x, motorBike.transform.position.y, -9);
+        _camera.transform.position = cameraPos;
+    }
+    void CameraFollowLerpBad() //test feature
+    {
+        Vector3 storage = _camera.transform.position;
+        storage.y = Mathf.Lerp(_camera.transform.position.y, motorBike.transform.position.y, 0.3f);
+        storage.x = Mathf.Lerp(_camera.transform.position.x, motorBike.transform.position.x, 0.3f);
+        _camera.transform.position = storage;
+    }
+    void CameraFollowLerbGood()
+    {
+        Vector3 current = _camera.transform.position;
+        Vector3 target = new Vector3(motorBike.transform.position.x, motorBike.transform.position.y, _camera.transform.position.z);
+        Vector3 lerp = Vector3.Lerp(current, target, CameraSpeed * Time.deltaTime);
+        _camera.transform.position = lerp;
     }
 }
