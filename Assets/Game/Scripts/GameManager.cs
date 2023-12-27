e002d9ae7b2b3827af6d827448621434c6caf4e9
Itt0ushura <balashov580@gmail.com>
2023-12-27 18:10:39 +0200

    Controller and camera follow for bike

--- Assets/Game/Scripts/GameManager.cs
+++ Assets/Game/Scripts/GameManager.cs
@@ -3,35 +3,52 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject motorBike; //gameobj of bike to manipulate with it
+    [SerializeField] private WheelJoint2D wheelJoint;
+    private JointMotor2D _motor; //var for manipulating with motorSpeed of wheel
     private Camera _camera;
     public float CameraSpeed; // var for t component in Vector3.Lerp
+
+
+    [SerializeField] private Rigidbody2D rb;
     void Start()
     {
         _camera = Camera.main;
     }
+    private void FixedUpdate()
+    {
+        RideControl();
+    }
     void LateUpdate()
     {
-        //CameraFollowRaw();
-        //CameraFollowLerpBad();
-        CameraFollowLerbGood();
+        CameraFollow();
     }
-    void CameraFollowRaw() //test feature
+    void CameraFollow()
     {
-        Vector3 cameraPos = new Vector3(motorBike.transform.position.x, motorBike.transform.position.y, -9);
-        _camera.transform.position = cameraPos;
+        Vector3 currentCamPosition = _camera.transform.position;
+        Vector3 targetCamPosition = new Vector3(motorBike.transform.position.x, motorBike.transform.position.y, _camera.transform.position.z);
+        Vector3 newCamPosition = Vector3.Lerp(currentCamPosition, targetCamPosition, CameraSpeed * Time.deltaTime);
+        _camera.transform.position = newCamPosition;
     }
-    void CameraFollowLerpBad() //test feature
+    void RideControl()
     {
-        Vector3 storage = _camera.transform.position;
-        storage.y = Mathf.Lerp(_camera.transform.position.y, motorBike.transform.position.y, 0.3f);
-        storage.x = Mathf.Lerp(_camera.transform.position.x, motorBike.transform.position.x, 0.3f);
-        _camera.transform.position = storage;
-    }
-    void CameraFollowLerbGood()
-    {
-        Vector3 current = _camera.transform.position;
-        Vector3 target = new Vector3(motorBike.transform.position.x, motorBike.transform.position.y, _camera.transform.position.z);
-        Vector3 lerp = Vector3.Lerp(current, target, CameraSpeed * Time.deltaTime);
-        _camera.transform.position = lerp;
+        if (Input.touchCount > 0)
+        {
+            _motor.maxMotorTorque = 2000;
+            if (Input.GetTouch(0).position.x > Screen.width / 2)
+            {
+                _motor.motorSpeed = Mathf.Lerp(_motor.motorSpeed, -1000f, Time.fixedDeltaTime);
+                wheelJoint.motor = _motor;
+            }
+            if (Input.GetTouch(0).position.x < Screen.width / 2)
+            {
+                _motor.motorSpeed = Mathf.Lerp(_motor.motorSpeed, 1000f, Time.fixedDeltaTime);
+                wheelJoint.motor = _motor;
+            }
+        }
+        else
+        {
+            _motor.motorSpeed = Mathf.Lerp(_motor.motorSpeed, 0f, Time.fixedDeltaTime);
+            wheelJoint.motor = _motor;
+        }
     }
 }
